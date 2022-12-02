039d5f555c4fe019bf66112b85f86469f5b74e4b
Praise Bakare <112479953+Praisebakare@users.noreply.github.com>
2022-12-02 10:30:52 +0100

    Create auth.js

---
+++ controller/auth.js
@@ -0,0 +1,153 @@
+const express = require("express")
+const app = express()
+const User = require("../models/user")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+require("dotenv").config()
+const localStorage = require("localStorage")
+
+
+var sess;
+
+//User Login
+const authUser = async(req, res) => {
+    const { username, password } = req.body
+    const user = await User.findOne({username}).lean()
+
+    if (!user) {
+        return res.json({status: 'error', error: 'Invalid Username/Password'})
+    }
+
+    if(await bcrypt.compare(password, user.password)) {
+
+        const token = jwt.sign(
+            { 
+                id: user._id, 
+                username: user.username
+            }, 
+            process.env.JWT
+        )
+        sess = req.session
+        sess.user = token
+        localStorage.setItem('user', username)
+        return res.json({status:'ok', data: token})
+        /* res.redirect('/home') */
+    }
+    res.json({status:'error', error: 'Invalid username/pasword'})
+}
+
+
+//User Registration
+const registerUser = async(req, res) => {
+    const {firstname, lastname, username, password: plainTextPassword, reEnterPassword} = req.body
+
+    if(!username || typeof username !== 'string') {
+        return res.json({
+            status:'error',
+            error: 'Invalid Username'
+        })
+    }
+
+    if(plainTextPassword != reEnterPassword) {
+        return res.json({
+            status: 'error',
+            error: 'Password Does not Match'
+        })
+    }
+
+    if(!plainTextPassword || typeof plainTextPassword !== 'string') {
+        return res.json({
+            status:'error',
+            error: 'Invalid Password'
+        })
+    }
+
+    if(plainTextPassword.length < 8) {
+        return res.json({
+            status:'error', 
+            error: 'Password should be atleast 8'
+        })
+    }
+
+    const password = await bcrypt.hash(plainTextPassword, 10)
+
+    try {
+        await User.create ({
+            firstname,
+            lastname,
+            username,
+            password
+        })
+    } catch (error) {
+        if (error.code === 11000) {
+            return res.json({status:'error', error: 'Username Already in use'})
+        } else {
+            console.log(error)
+            return res.json({status:'error', error: 'An error occurred during registration. Try again Later'})
+        }
+    }
+    res.json({status:'OK'})
+}
+
+//User Logout
+const logoutUser = (req, res) => {
+    req.session.destroy((err) => {
+        if(err) {
+            console.log(err)
+            return res.json({status:'error', error: 'An error occurred during signout. Try again Later'})
+        }
+        res.redirect('/user/login')
+    })
+}
+
+
+//Change Password
+const changePassword = async(req, res) => {
+    let sess = req.session
+    if (sess.user) {
+        const {newPassword:plainTextPassword, reEnterPassword} = req.body
+
+        const token = sess.user
+
+        if(plainTextPassword.length < 8) {
+            return res.json({
+                status:'error', 
+                error: 'Password should be atleast 8'
+            })
+        }
+
+        if(plainTextPassword == reEnterPassword) {
+            return res.json({
+                status:'error', 
+                error: 'Password not the same'
+            })
+        }
+
+        try{
+            const user = jwt.verify(token, process.env.JWT)
+            const _id = user.id
+
+            const password = await bcrypt.hash(plainTextPassword, 10)
+
+            await User.updateOne(
+                {_id},
+                {
+                    $set: {password: password}
+                }
+            )
+            res.json({status: 'ok'})
+        } catch (error) {
+            console.log(error)
+            res.json({status:'error', error: 'Contact the Administrator'})
+        }
+    } else {
+        res.redirect('/user/login')
+    }
+}
+
+module.exports = {
+    authUser, 
+    registerUser, 
+    logoutUser,
+    changePassword
+}
