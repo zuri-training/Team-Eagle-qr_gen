30afeebbc2b6bf88e92858ceba16543f06025c85
Praise Bakare <112479953+Praisebakare@users.noreply.github.com>
2022-12-02 11:20:44 +0100

    Update qrcode.js

--- controller/qrcode.js
+++ controller/qrcode.js
@@ -16,12 +16,32 @@ const qrcodegen = (req, res) => {
     } else {
         res.redirect('/user/login')
     } */
+    let sess = req.session
     const url = req.body.url;
     if (url.length === 0) res.send("Empty Data!")
         QRCode.toString(url,{type:'terminal'}, function (err, QRcode) {
         
         if(err) return console.log("error occurred")
         console.log(QRcode)
+        try {
+            var obj = {
+                username: sess.user,
+                img: {
+                    data: fs.readFileSync(path.join(__dirname + '/uploads/' + url)),
+                    contentType: 'image/png'
+                }
+            }
+            image.create(obj, (err, item) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    res.redirect('/User/Created');
+                }
+            });
+        } catch (error) {
+            res.render('create', {message: "An error occurred"})
+        }
     })
 
     QRCode.toDataURL(url, (err, src) => {
