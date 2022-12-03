40eca63b0f0536a484792fcdd5510e586bf90327
Praise Bakare <112479953+Praisebakare@users.noreply.github.com>
2022-12-03 08:02:31 +0100

    Update qrcode.js

--- controller/qrcode.js
+++ controller/qrcode.js
@@ -9,8 +9,6 @@ const localStorage = require("localStorage")
 
 var sess
 
-
-
 const qrcodegen = async (req, res) => {
     let sess = req.session
     const { id: qrcodetype } = req.params
@@ -27,44 +25,55 @@ const qrcodegen = async (req, res) => {
 
     if(qrcodetype == 'menu') {
         let data = {
-            link: req.body.url
-        }
-         
-        let details = JSON.stringify(data)
-    
-        if (details.length === 0) res.send("Empty Data!")
-        QRCode.toString(details,opts, function (err, QRcode) {
-            if(err) return console.log("error occurred")
-            console.log(QRcode)
-            res.json({status: "ok"})
-        })
-    } else if(qrcodetype == 'card') {
-        let data = {
             link: req.body.url,
             name: req.body.name,
-            jobTitle: req.body.name,
+            menu: req.body.menu,
             website: req.body.website,
         }
          
         let details = JSON.stringify(data)
     
         if (details.length === 0) res.send("Empty Data!")
-        QRCode.toString(details,opts, function (err, QRcode) {
+        QRCode.toDataURL(details, opts, function(err, QRcode) {
             if(err) return console.log("error occurred")
             console.log(QRcode)
-            res.json({status: "ok", data})
+
+            //QRcode is store as base64
+            res.json({status: "ok", QRcode})
         })
+    } else if(qrcodetype == 'card') {
+        let data = {
+            link: req.body.url,
+            name: req.body.name,
+            jobTitle: req.body.jobTitle,
+            website: req.body.website
+        }
+         
+        let details = JSON.stringify(data)
+    
+        if (details.length === 0) res.send("Empty Data!")
+
+        QRCode.toDataURL(details, opts, function(err, QRcode) {
+            if(err) return console.log("error occurred")
+            console.log(QRcode)
+
+            //QRcode is store as base64
+            res.json({status: "ok", QRcode})
+        })
+
     } else if(qrcodetype == 'web'){
         const details = req.body.url
     
         if (details.length === 0) res.send("Empty Data!")
-        QRCode.toString(details,opts, function (err, QRcode) {
+        QRCode.toDataURL(details, opts, function(err, QRcode) {
             if(err) return console.log("error occurred")
             console.log(QRcode)
-            res.json({status: "ok"})
+
+            //QRcode is store as base64
+            res.json({status: "ok", QRcode})
         })
     }
-}
+}   
 
 const getAllqrCode = (req, res) => {
     let sess = req.session
