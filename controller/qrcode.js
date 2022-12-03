da1e56a73ec02c46ef893036d63c5ce80cd5a1b0
Praise Bakare <112479953+Praisebakare@users.noreply.github.com>
2022-12-03 19:20:29 +0100

    Update qrcode.js

--- controller/qrcode.js
+++ controller/qrcode.js
@@ -1,6 +1,5 @@
 const qrcode = require("../models/qrcode")
 const QRCode = require("qrcode")
-const multer = require("multer")
 const bcrypt = require("bcrypt")
 
 var sess
@@ -8,150 +7,92 @@ var sess
 const qrcodegen = async (req, res) => {
     let sess = req.session
 
-    const opts = {
-        errorCorrectionLevel: 'H',
-        type: 'terminal',
-        quality: 0.95,
-        color: {
-         dark: '#208698',
-         light: '#FFF',
-        },
-    }
-
-    const details = req.body.text
-    const password = req.body.password
-
-    if(password === undefined) {
-        if (details.length === 0) res.send("Empty Data!")
-        QRCode.toDataURL(details, opts, function(err, QRcode) {
-            if(err) return console.log("error occurred")
-            /* console.log(QRcode) */
-
-            //QRcode is store as base64
-            qrcode.create({
-                user: sess.user,
-                img: {
-                    data: QRcode,
-                    contentType:  'image/png'
-                }
+    if (sess.user) {
+        const opts = {
+            errorCorrectionLevel: 'H',
+            type: 'terminal',
+            quality: 0.95,
+            color: {
+             dark: '#208698',
+             light: '#FFF',
+            },
+        }
+    
+        const details = req.body.text
+        const password = req.body.password
+    
+        if(password === undefined) {
+            if (details.length === 0) res.send("Empty Data!")
+            QRCode.toDataURL(details, (err, QRcode) => {
+                if(err) return console.log("error occurred")
+                /* console.log(QRcode) */
+    
+                //QRcode is store as base64
+                
+                qrcode.create({
+                    user: sess.user,
+                    img: {
+                        data: QRcode,
+                        contentType:  'image/png'
+                    }
+                })
+                res.json({status: "ok", QRcode})
             })
-            res.json({status: "ok", QRcode})
-        })
-        /* QRCode.toString(details,opts,
-            function (err, QRcode) {
-
-            if(err) return console.log("error occurred")
-
-            console.log(QRcode)
-        }) */
+        } else {
+            const passwordd = await bcrypt.hash(password, 10)
+    
+            const protected = {
+                data: details,
+                key: passwordd
+            }
+    
+            const protectedcode = JSON.stringify(protected)
+    
+            if (protectedcode.length === 0) res.send("Empty Data!")
+    
+            QRCode.toDataURL(protectedcode, (err, QRcode) => {
+                if(err) return console.log("error occurred")
+                /* console.log(QRcode) */
+    
+                //QRcode is store as base64
+                qrcode.create({
+                    user: sess.user,
+                    img: {
+                        data: QRcode,
+                        contentType:  'image/png'
+                    }
+                })
+                res.json({status: "ok", QRcode})
+            })
+        }
     } else {
-        const passwordd = await bcrypt.hash(password, 10)
-
-        const protected = {
-            data: details,
-            key: passwordd
-        }
-
-        const protectedcode = JSON.stringify(protected)
-
-        if (protectedcode.length === 0) res.send("Empty Data!")
-
-        QRCode.toDataURL(protectedcode, opts, function(err, QRcode) {
-            if(err) return console.log("error occurred")
-            /* console.log(QRcode) */
-
-            //QRcode is store as base64
-            res.json({status: "ok", QRcode})
-
-            qrcode.create({
-                user: sess.user,
-                img: {
-                    data: QRcode,
-                    contentType:  'image/png'
-                }
-            })
-        })
-       /*  QRCode.toString(protectedcode,opts,
-                        function (err, QRcode) {
-
-        if(err) return console.log("error occurred")
-
-        console.log(QRcode)
-    }) */
+        res.redirect('/user/login')
     }
-
-    /* if(qrcodetype == 'menu') {
-        let data = {
-            link: req.body.url,
-            name: req.body.name,
-            menu: req.body.menu,
-            website: req.body.website,
-        }
-         
-        let details = JSON.stringify(data)
-    
-        if (details.length === 0) res.send("Empty Data!")
-        QRCode.toDataURL(details, opts, function(err, QRcode) {
-            if(err) return console.log("error occurred")
-            console.log(QRcode)
-
-            //QRcode is store as base64
-            res.json({status: "ok", QRcode})
-        })
-    } else if(qrcodetype == 'card') {
-        let data = {
-            link: req.body.url,
-            name: req.body.name,
-            jobTitle: req.body.jobTitle,
-            website: req.body.website
-        }
-         
-        let details = JSON.stringify(data)
-    
-        if (details.length === 0) res.send("Empty Data!")
-
-        QRCode.toDataURL(details, opts, function(err, QRcode) {
-            if(err) return console.log("error occurred")
-            console.log(QRcode)
-
-            //QRcode is store as base64
-            res.json({status: "ok", QRcode})
-        })
-
-    } else if(qrcodetype == 'web'){
-        const details = req.body.url
-    
-        if (details.length === 0) res.send("Empty Data!")
-        QRCode.toDataURL(details, opts, function(err, QRcode) {
-            if(err) return console.log("error occurred")
-            console.log(QRcode)
-
-            //QRcode is store as base64
-            res.json({status: "ok", QRcode})
-        })
-    } */
 }   
 
 const getAllqrCode = (req, res) => {
     let sess = req.session
-    qrcode.find({user: sess.user}, (err, images) => {
-        if(err) return console.log("error occurred" + err)
-        res.json({status: 'OK', item: images})
-    })
-    /* if (sess.user) {
-        
+    if (sess.user) {
+        qrcode.find({user: sess.user}, (err, images) => {
+            if(err) return console.log("error occurred" + err)
+            res.json({status: 'OK', item: images})
+        })
     } else {
         res.redirect('/user/login')
-    } */
+    }
 }
 
 const getqrCode = (req, res) => {
     let sess = req.session
     const {id: itemID} = req.params
-    qrcode.findOne({user: sess.user, _id: itemID}, (err, image) => {
-        if(err) return console.log("error occurred" + err)
-        res.json({status: 'OK', item: image})
-    })
+    if(sess.user) {
+        qrcode.findOne({user: sess.user, _id: itemID}, (err, image) => {
+            if(err) return console.log("error occurred" + err)
+            res.json({status: 'OK', item: image})
+        })
+    } else {
+        res.redirect('/user/login')
+    }
 }
 
 module.exports = {
