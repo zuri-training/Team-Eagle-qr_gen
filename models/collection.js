eed6937ecd9ff8988565fe4b7f3c5963c6aa7ede
Praise Bakare <112479953+Praisebakare@users.noreply.github.com>
2022-12-04 20:46:32 +0100

    Update collection.js

--- models/collection.js
+++ models/collection.js
@@ -3,7 +3,20 @@ const mongoose = require('mongoose');
 const collectionSchema = new mongoose.Schema({
     userID: String,
     qrcodeID: String,
-
+    qrCodeType: String,
+    firstname: String,
+    lastname: String,
+    imageurl: String,
+    jobTitle: String,
+    businessName: String,
+    description: String,
+    productTitle: String,
+    CatalogTitle: String,
+    phonenumber: String,
+    websiteUrl: String,
+    openingDay: String,
+    OpenAndCloseTime: TimeRanges,
+    socialMedia: String
 });
   
 module.exports = new mongoose.model('collection', collectionSchema);
