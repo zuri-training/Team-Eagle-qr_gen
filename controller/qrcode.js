51e9b58cb1d035f16d3e3f007951b349b1a5eb0f
Praise Bakare <112479953+Praisebakare@users.noreply.github.com>
2022-12-04 18:21:25 +0100

    Update qrcode.js

--- controller/qrcode.js
+++ controller/qrcode.js
@@ -1,71 +1,23 @@
 const express = require("express")
 const qrcode = require("../models/qrcode")
-const QRCode = require("qrcode")
-const bcrypt = require("bcrypt")
 const session = require("express-session")
+const cloudinary = require("cloudinary").v2
+require("dotenv").config()
 var sess
+
+cloudinary.config({ 
+    cloud_name: process.env.cloud_name, 
+    api_key: process.env.api_key, 
+    api_secret: process.env.api_secret 
+});
 
 const qrcodegen = async (req, res) => {
     let sess = req.session
 
     if (sess.user) {
-        const details = req.body.text
-        const password = req.body.password
-        
-        const opts = {
-            errorCorrectionLevel: 'H',
-            type: 'terminal',
-            quality: 0.95,
-            color: {
-                dark: '#208698',
-                light: '#FFF',
-            },
-        }
-    
-        if(password === undefined) {
-            if (details.length === 0) res.send("Empty Data!")
-            QRCode.toDataURL(details,opts, (err, QRcode) => {
-                if(err) return console.log("error occurred")
-                /* console.log(QRcode) */
-    
-                //QRcode is store as base64
-                
-                qrcode.create({
-                    user: sess.user,
-                    img: {
-                        data: QRcode,
-                        contentType:  'image/png'
-                    }
-                })
-                res.json({status: "ok", QRcode})
-            })
-        } else {
-            const passwordd = await bcrypt.hash(password, 10)
-    
-            const protected = {
-                data: details,
-                key: passwordd
-            }
-    
-            const protectedcode = JSON.stringify(protected)
-    
-            if (protectedcode.length === 0) res.send("Empty Data!")
-    
-            QRCode.toDataURL(protectedcode,opts, (err, QRcode) => {
-                if(err) return console.log("error occurred")
-                /* console.log(QRcode) */
-    
-                //QRcode is store as base64
-                qrcode.create({
-                    user: sess.user,
-                    img: {
-                        data: QRcode,
-                        contentType:  'image/png'
-                    }
-                })
-                res.json({status: "ok", QRcode})
-            })
-        }
+        cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+        { public_id: "olympic_flag" },  
+        function(error, result) {console.log(result); });
     } else {
         res.redirect('/user/login')
     }
@@ -74,10 +26,7 @@ const qrcodegen = async (req, res) => {
 const getAllqrCode = (req, res) => {
     let sess = req.session
     if (sess.user) {
-        qrcode.find({user: sess.user}, (err, images) => {
-            if(err) return console.log("error occurred" + err)
-            res.json({status: 'OK', images})
-        })
+       
     } else {
         res.redirect('/user/login')
     }
@@ -87,10 +36,7 @@ const getqrCode = (req, res) => {
     let sess = req.session
     const {id: itemID} = req.params
     if(sess.user) {
-        qrcode.findOne({user: sess.user, _id: itemID}, (err, image) => {
-            if(err) return console.log("error occurred" + err)
-            res.json({status: 'OK', image})
-        })
+        
     } else {
         res.redirect('/user/login')
     }
