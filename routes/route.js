f3aede8ec384dedbf309f117ffb8f08896fe3c4f
Praise Bakare <112479953+Praisebakare@users.noreply.github.com>
2022-12-06 22:57:37 +0100

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -1,18 +1,5 @@
 const express = require("express")
 const router = express.Router()
-const multer = require("multer")
-
-
-var storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'uploads')
-	},
-	filename: (req, file, cb) => {
-		cb(null, file.fieldname + '-' + Date.now())
-	}
-});
-
-var upload = multer({ storage: storage });
 
 const {
     authUser,
